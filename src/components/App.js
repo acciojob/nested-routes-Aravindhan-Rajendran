75d1fa0ec7254c64658d0bee8a8ddd16ff112a50
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-31 23:41:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import Category from './Category';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="category/:category" element={<Category />} />
+      </Route>
+    </Routes>
+  </Router>
+);
 
-export default App
+export default App;
